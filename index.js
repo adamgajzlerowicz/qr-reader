304a445905a162d314e5e7a7016dfbd33ca9b0b8
Adam Gajzlerowicz <nelf86@gmail.com>
2019-02-22 09:05:07 +0100

    added eslint

--- index.js
+++ index.js
@@ -4,67 +4,44 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
+import React, { Component } from "react";
+import { name as appName } from "./app.json";
+import { AppRegistry, ToastAndroid, Button, View } from "react-native";
 
-import React, { Component } from 'react';
-import {name as appName} from './app.json';
-import {
-    AppRegistry,
-    StyleSheet,
-    ToastAndroid,
-    Button,
-    View
-} from 'react-native';
-
-
-
-
-import QRCodeScanner from 'react-native-qrcode-scanner';
+import QRCodeScanner from "react-native-qrcode-scanner";
 
 class ScanScreen extends Component<{}, { isOpen: boolean }> {
-    state = {
-        isOpen: false
-    }
+  state = {
+    isOpen: false
+  };
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <View>
-                {this.state.isOpen && <QRCodeScanner
-                    cameraProps={{
-                        captureAudio: false
-                    }}
-                onRead={e => {
-                    this.setState({ isOpen: false })
-                    ToastAndroid.show(e.data, ToastAndroid.SHORT);
-                }}
-            />}
-                { !this.state.isOpen && <Button onPress={() => this.setState({isOpen: true})} title={"open"}/>}
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        {this.state.isOpen && (
+          <QRCodeScanner
+            cameraProps={{
+              captureAudio: false
+            }}
+            onRead={e => {
+              this.setState({ isOpen: false });
+              ToastAndroid.show(e.data, ToastAndroid.SHORT);
+            }}
+          />
+        )}
+        {!this.state.isOpen && (
+          <Button
+            onPress={() => this.setState({ isOpen: true })}
+            title={"open"}
+          />
+        )}
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-    centerText: {
-        flex: 1,
-        fontSize: 18,
-        padding: 32,
-        color: '#777',
-    },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    buttonText: {
-        fontSize: 21,
-        color: 'rgb(0,122,255)',
-    },
-    buttonTouchable: {
-        padding: 16,
-    },
-});
 
 AppRegistry.registerComponent(appName, () => ScanScreen);
