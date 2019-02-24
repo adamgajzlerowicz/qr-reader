298e8ab088ff1251248e71aca898014f1781052d
Adam Gajzlerowicz <nelf86@gmail.com>
2019-02-24 10:43:19 +0100

    added eye candy

--- index.js
+++ index.js
@@ -6,13 +6,61 @@
 
 import React from "react";
 import { name as appName } from "./app.json";
-import { StyleSheet, AppRegistry, ToastAndroid, View } from "react-native";
-import { Button } from "teaset";
+import {
+  StyleSheet,
+  AppRegistry,
+  Button,
+  ToastAndroid,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 import QRCodeScanner from "react-native-qrcode-scanner";
 
-class ScanScreen extends React.Component<{}, { isOpen: boolean }> {
+const styles = StyleSheet.create({
+  button: {
+    width: "100%"
+  },
+
+  scroll: {
+    width: "100%"
+  },
+
+  heading: {
+    fontSize: 50,
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+    textDecorationColor: "#000",
+    marginBottom: 24
+  },
+
+  drive: {
+    fontSize: 24,
+    textAlign: "left",
+    width: "100%"
+  },
+
+  container: {
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "white",
+    flex: 1,
+    width: "100%",
+    padding: 20
+  }
+});
+
+type StateType = {
+  isOpen: boolean,
+  drives: string[]
+};
+
+class ScanScreen extends React.Component<{}, StateType> {
   state = {
-    isOpen: false
+    isOpen: false,
+    drives: []
   };
 
   constructor(props) {
@@ -23,38 +71,48 @@ class ScanScreen extends React.Component<{}, { isOpen: boolean }> {
     return (
       <View style={styles.container}>
         {this.state.isOpen && (
-          <QRCodeScanner
-            cameraProps={{
-              captureAudio: false
-            }}
-            onRead={e => {
-              this.setState({ isOpen: false });
-              ToastAndroid.show(e.data, ToastAndroid.SHORT);
-            }}
-          />
+          <View>
+            <QRCodeScanner
+              cameraProps={{
+                captureAudio: false
+              }}
+              onRead={e => {
+                this.setState({
+                  isOpen: false,
+                  drives: [e.data, ...this.state.drives]
+                });
+                ToastAndroid.show(e.data, ToastAndroid.SHORT);
+              }}
+            />
+            <Button
+              onPress={() => this.setState({ isOpen: false })}
+              title={"Cancel"}
+            />
+          </View>
         )}
+
         {!this.state.isOpen && (
-          <Button
-            onPress={() => this.setState({ isOpen: true })}
-            size={"lg"}
-            type={"primary"}
-            title={"Scan"}
-          />
+          <View style={styles.container}>
+            <Text style={styles.heading}>Previous drives:</Text>
+
+            <ScrollView centerContent={false} style={styles.scroll}>
+              {this.state.drives.map((drive, index) => (
+                <Text key={index} style={styles.drive}>
+                  {drive}
+                </Text>
+              ))}
+            </ScrollView>
+            <TouchableOpacity style={styles.button}>
+              <Button
+                onPress={() => this.setState({ isOpen: true })}
+                title={"Scan"}
+              />
+            </TouchableOpacity>
+          </View>
         )}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    backgroundColor: "white",
-    height: "100%",
-    padding: 20
-  }
-});
 
 AppRegistry.registerComponent(appName, () => ScanScreen);
